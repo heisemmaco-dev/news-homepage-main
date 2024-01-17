190a344bf0eed8d9f2a22f72c5702b55ad598b03
heisemmaco-dev <heisemmaco@gmail.com>
2024-01-17 10:23:28 +0100

    added some animation to my challenge

--- main.js
+++ main.js
@@ -2,7 +2,30 @@
 let header = document.querySelector('header')
 // the menu button
 let menuButton = document.querySelector('.toggler')
+let nav = document.querySelector('nav')
 // add event listener to the button
-menuButton.addEventListener('click', () => {
+/* menuButton.addEventListener('click', () => {
     header.classList.toggle('showHiddenItem')
-})
+    nav.classList.toggle('slice-out')
+    nav.classList.toggle('slide-in')
+}) */
+
+menuButton.addEventListener('click', () => {
+    if (!header.classList.contains('showHiddenItem')) {
+     /*  slideContainer.classList.remove('hidden');
+      slideContainer.classList.add('slide-in'); */
+      header.classList.add('showHiddenItem')
+      nav.classList.remove('slide-out')
+      nav.classList.add('slide-in')
+    } else {
+        console.log('hi, you are welcome');
+        
+     
+        nav.classList.remove('slide-in')
+        nav.classList.add('slide-out')
+
+      setTimeout(() => {
+        header.classList.remove('showHiddenItem')
+      }, 1000); 
+    }
+  });
